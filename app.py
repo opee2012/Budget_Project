2b2ea47b744f66f7183dae3197b8bd88bf1960c7
opee2012 <jborth2010@gmail.com>
2023-02-20 16:38:58 -0500

    Added a budgets.html and login.html page. Still WIP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from bson.objectid import ObjectId
 import db_details
 
@@ -29,9 +29,43 @@ expense = db.expense
 #     return redirect(url_for('index'))
 
 
-@app.route('/', methods=('GET', 'POST'))
-def index():
+@app.route('/')
+def home():
     return render_template('home.html')
+
+
+@app.route('/budgets')
+def budgets():
+    if 'username' in session:
+        return render_template('pages/budgets.html')
+    else:
+        # Redirect to the login page if the user is not logged in
+        return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Check if the username and password are correct
+        if request.form['username'] == 'your_username' and request.form['password'] == 'your_password':
+            session['username'] = request.form['username']
+            # Redirect to the Budgets page if the login is successful
+            return redirect(url_for('budgets'))
+        else:
+            # Display an error message if the login is unsuccessful
+            error = 'Invalid username or password. Please try again.'
+            return render_template('pages/login.html', error=error)
+    else:
+        return render_template('pages/login.html')
+
+
+# Define a route for logging out
+@app.route('/logout')
+def logout():
+    # Remove the username from the session if it exists
+    session.pop('username', None)
+    # Redirect to the home page
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
